68ef1715e16a07e8cb9b6b46fa1b5eb46f19cb53
fermatrix91 <fjosuemn@gmail.com>
2016-08-17 21:52:43 -0600

    ErpInicial
    App Básica

---
+++ MontesERP/Models/Contabilidad/ComprobantesDiario.cs
@@ -0,0 +1,115 @@
+﻿using MontesERP.Models.DAL;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MontesERP.Models.Contabilidad
+{
+    public class ComprobantesDiario
+    {
+        public readonly MontesERPEntities modeloErp = new MontesERPEntities();
+
+        public List<ComprobanteObj> ListadoDeComprobantes()
+        {
+            List<ComprobanteObj> listaDeComprobantes = new List<ComprobanteObj>();
+
+            var consultaComprobantes = (from comprobante in modeloErp.ComprobanteDeDiarios
+                                        select comprobante).ToList();
+
+            foreach (ComprobanteDeDiario comprobante in consultaComprobantes)
+            {
+                listaDeComprobantes.Add(new ComprobanteObj
+                {
+                    IdComprobante = comprobante.IdComprobanteDeDiario,
+                    Concepto = comprobante.Concepto,
+                    FechaInicial = Convert.ToDateTime(comprobante.FechaInicial).ToShortDateString(),
+                    FechaCierre = Convert.ToDateTime(comprobante.FechaCierre).ToShortDateString()
+                });
+            }
+
+            return listaDeComprobantes;
+        }
+
+        public DetalleDeComprobante DetalleDeComprobante(int idComprobante)
+        {
+            DetalleDeComprobante listaElementoComprobante = new DetalleDeComprobante();
+
+            var consultaComprobante = (from comprobant in modeloErp.ComprobanteDeDiarios
+                                       where comprobant.IdComprobanteDeDiario == idComprobante
+                                       select comprobant).FirstOrDefault();
+
+            if (consultaComprobante != null)
+            {
+                listaElementoComprobante.Comprobante = new ComprobanteObj
+                {
+                    IdComprobante = consultaComprobante.IdComprobanteDeDiario,
+                    NumeroFolio = consultaComprobante.NumeroFolio,
+                    Concepto = consultaComprobante.Concepto,
+                    FechaInicial = Convert.ToDateTime(consultaComprobante.FechaInicial).ToShortDateString()
+                };
+
+                var consultaDetalle = (from comprobant in modeloErp.DetalleComprobanteDeDiarios
+                    where comprobant.IdComprobanteDeDiario == idComprobante
+                    select comprobant).ToList();
+
+                if (consultaDetalle.Count != 0)
+                {
+                    listaElementoComprobante.DetalleComprobante = new List<ElementoComprobante>();
+                    foreach (DetalleComprobanteDeDiario detalle in consultaDetalle)
+                    {
+                        listaElementoComprobante.DetalleComprobante.Add(new ElementoComprobante
+                        {
+                            IdSubCuenta = detalle.IdSubCuenta.ToString(),
+                            NombreSubCuenta = detalle.SubCuenta.NombreSubCuenta,
+                            Debe = detalle.Debe.ToString(),
+                            Haber = detalle.Haber.ToString()
+                        });
+                    }
+                }
+            }
+            else
+            {
+                listaElementoComprobante.Comprobante = new ComprobanteObj
+                {
+                    IdComprobante = 0,
+                    NumeroFolio = 1,
+                    Concepto = "",
+                    FechaInicial = DateTime.Now.ToShortDateString()
+                };
+            }
+
+            return listaElementoComprobante;
+        }
+    }
+
+    public class DetalleDeComprobante
+    {
+        public ComprobanteObj Comprobante { get; set; }
+        public List<ElementoComprobante> DetalleComprobante { get; set; }
+    }
+
+    public class ElementoComprobante
+    {
+        public string IdSubCuenta { get; set; }
+
+        public string NombreSubCuenta { get; set; }
+
+        public string Debe { get; set; }
+
+        public string Haber { get; set; }
+    }
+
+    public class ComprobanteObj
+    {
+        public int IdComprobante { get; set; }
+        public int NumeroFolio { get; set; }
+
+        public string Concepto { get; set; }
+
+        public string FechaInicial { get; set; }
+
+        public string FechaCierre { get; set; }
+    }
+}
---
+++ MontesERP/Models/Contabilidad/Cuentas.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MontesERP.Models.DAL;
+
+namespace MontesERP.Models.Contabilidad
+{
+    public class Cuentas
+    {
+        public readonly MontesERPEntities modeloErp = new MontesERPEntities();
+
+        public List<CuentaObj> ListadoDeCuentas()
+        {
+            List<CuentaObj> listaDeCuentas = new List<CuentaObj>();
+
+            var consultaCuentas = (from cuenta in modeloErp.Cuentas
+                                   select cuenta).ToList();
+
+            foreach (Cuenta cuenta in consultaCuentas)
+            {
+                listaDeCuentas.Add(new CuentaObj
+                {
+                    Grupo = cuenta.SubGrupoCuenta.GrupoCuenta.NombreGrupoCuenta,
+                    SubGrupo = cuenta.SubGrupoCuenta.NombreSubGrupoCuenta,
+                    IdCuenta = cuenta.IdCuenta,
+                    Cuenta = cuenta.NombreCuenta
+                });
+            }
+
+            return listaDeCuentas;
+        }
+
+        public List<SubCuentaObj> ListadoDeSubCuentas(int idCuenta)
+        {
+            List<SubCuentaObj> listaDeSubCuentas = new List<SubCuentaObj>();
+
+            var consultaCuentas = (from subCuenta in modeloErp.SubCuentas
+                                   where subCuenta.IdCuenta == idCuenta
+                                   select subCuenta).ToList();
+
+            foreach (SubCuenta cuenta in consultaCuentas)
+            {
+                listaDeSubCuentas.Add(new SubCuentaObj
+                {
+                    IdSubCuenta = cuenta.IdSubCuenta,
+                    SubCuenta = cuenta.NombreSubCuenta
+                });
+            }
+
+            return listaDeSubCuentas;
+        }
+
+        public List<SubCuentaObj> ListadoTotalDeSubCuentas()
+        {
+            List<SubCuentaObj> listaDeSubCuentas = new List<SubCuentaObj>();
+
+            var consultaCuentas = (from subCuenta in modeloErp.SubCuentas
+                                   select subCuenta).ToList();
+
+            foreach (SubCuenta cuenta in consultaCuentas)
+            {
+                listaDeSubCuentas.Add(new SubCuentaObj
+                {
+                    IdSubCuenta = cuenta.IdSubCuenta,
+                    SubCuenta = cuenta.NombreSubCuenta
+                });
+            }
+
+            return listaDeSubCuentas;
+        }
+
+        public ArrayList DetalleDeSubCuenta(int idSubCuenta)
+        {
+            ArrayList subCuenta = new ArrayList();
+
+            var consultaSubCuenta = (from subCuent in modeloErp.SubCuentas
+                                     where subCuent.IdSubCuenta == idSubCuenta
+                                     select subCuent).FirstOrDefault();
+
+            if (consultaSubCuenta != null)
+                subCuenta.Add(new
+                {
+                    consultaSubCuenta.IdSubCuenta,
+                    consultaSubCuenta.IdCuenta,
+                    consultaSubCuenta.NombreSubCuenta
+                });
+
+            return subCuenta;
+        }
+    }
+
+    public class CuentaObj
+    {
+        public string Grupo { get; set; }
+
+        public string SubGrupo { get; set; }
+
+        public int IdCuenta { get; set; }
+
+        public string Cuenta { get; set; }
+    }
+
+    public class SubCuentaObj
+    {
+        public int IdSubCuenta { get; set; }
+
+        public string SubCuenta { get; set; }
+    }
+}
